389757cc6666d11e6eb2ed7e8acb6258af844bdc
ryabuily <ryabuily@fit.cvut.cz>
2020-11-01 01:07:07 +0100

    user microservice substituted with customer

--- backend/rest-user/src/test/java/hackprague/backend/quarkus/NativeUserResourceIT.java
+++
@@ -1,9 +0,0 @@
-package hackprague.backend.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeUserResourceIT extends UserResourceTest {
-
-    // Execute the same tests but in native mode.
-}
